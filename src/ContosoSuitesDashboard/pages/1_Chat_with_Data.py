6fe43c1cfd8cc2a2fd2ab9f7cf5d2dc7977cd9d4
Sepehr Sepehri <54600808+SepehrSepehri@users.noreply.github.com>
2025-04-08 21:05:43 +0000

    ex1

--- src/ContosoSuitesDashboard/pages/1_Chat_with_Data.py
+++ src/ContosoSuitesDashboard/pages/1_Chat_with_Data.py
@@ -18,6 +18,9 @@ def create_chat_completion(messages):
     )
     
     aoai_endpoint = st.secrets["aoai"]["endpoint"]
+    search_endpoint = st.secrets["search"]["endpoint"]
+    search_key = st.secrets["search"]["key"]
+    search_index_name = st.secrets["search"]["index_name"]
     aoai_deployment_name = st.secrets["aoai"]["deployment_name"]
 
     client = openai.AzureOpenAI(
@@ -32,7 +35,22 @@ def create_chat_completion(messages):
             {"role": m["role"], "content": m["content"]}
             for m in messages
         ],
-        stream=True
+        stream=True,
+        extra_body={
+              "data_sources": [
+                  {
+                      "type": "azure_search",
+                      "parameters": {
+                          "endpoint": search_endpoint,
+                          "index_name": search_index_name,
+                          "authentication": {
+                              "type": "api_key",
+                              "key": search_key
+                          }
+                      }
+                  }
+              ]
+          }
     )
 
 def handle_chat_prompt(prompt):
